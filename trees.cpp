2f8ba9ff71dc69926efd0316a4224ae11564e1a9
Aveek Chakraborty <aveek.github@gmail.com>
2024-08-28 22:35:56 +0530

    changes

--- trees.cpp
+++ trees.cpp
@@ -38,6 +38,69 @@ void postorder(Node* root){
     cout<<root->val<<" ";
 }
 
+void levelorder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        auto node=q.front();
+        q.pop();
+        cout<<node->val<<" ";
+
+        if(node->left){
+            q.push(node->left);
+        }
+
+        if(node->right){
+            q.push(node->right);
+        }
+    }
+}
+
+int height(Node* root){
+    if(!root) return 0;
+
+    int l = height(root->left);
+    int r = height(root->right);
+
+    return 1+max(l,r);
+}
+
+int isbalanced(Node* root){
+    if(!root) return 0;
+
+    int l = isbalanced(root->left);
+    if(l==-1) return -1;
+
+    int r = isbalanced(root->right);
+    if(r==-1) return -1;
+
+    if(abs(l-r)>-1) return -1;
+
+    return 1+max(l,r);
+}
+
+int maxi=0;
+int diameter(Node* root){
+    if(!root) return 0;
+
+    int l = diameter(root->left);
+    int r = diameter(root->right);
+
+    int maxi = max(maxi,l+r);
+
+    return 1+max(l,r);
+}
+
+
+bool same(Node* p , Node* q){
+    if(!p && !q) return true;
+    if(!p || !q) return false;
+
+    return (p->val == q->val && same(p->left,q->left) && same(p->right,q->right));
+}
+
+
 
 
 
@@ -53,16 +116,40 @@ int main(){
     templ->left = new Node(4);
     templ->right = new Node(5);
 
-    // templ->right->right = new Node(8); 
-    // templ->right->right->right = new Node(9); 
+    templ->right->right = new Node(8); 
+    templ->right->right->right = new Node(99); 
 
     tempr->right = new Node(7);
     tempr->left = new Node(6);
 
 
-    inorder(root);cout<<endl;
-    preorder(root);cout<<endl;
-    postorder(root);cout<<endl;
+    Node* root2 = new Node(1);
+    root2->left = new Node(2);
+    root2->right = new Node(3);
+
+    Node* templ2 =  root2->left;
+    Node* tempr2 = root2->right;
+
+    templ2->left = new Node(4);
+    templ2->right = new Node(5);
+
+    templ2->right->right = new Node(8); 
+    templ2->right->right->right = new Node(9); 
+
+    tempr2->right = new Node(7);
+    tempr2->left = new Node(6);
+
+
+    // inorder(root);cout<<endl;
+    // preorder(root);cout<<endl;
+    // postorder(root);cout<<endl;
+
+
+    // levelorder(root);
+
+    // cout<<isbalanced(root);
+
+    cout<<same(root , root2);
 
     return 0;
 }
